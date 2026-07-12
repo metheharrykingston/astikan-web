4ba2b067ae55cdb6208349cefd1e24ed087e4cab
metheharrykingston <meharrykingston@gmail.com>
2026-07-13 00:56:51 +0530

    Load Astikan desktop layout fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'aos/dist/aos.css';
 import './index.css';
+import './desktop-fixes.css';
 import RootApp from './RootApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
